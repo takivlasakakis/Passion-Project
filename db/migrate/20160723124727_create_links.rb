9d9847fd3c60628731b1ee6da71fd007d5c239a7
TakiVlasakakis <email": null>
2016-07-23 13:13:19 -0700

    wrote the migration files

--- db/migrate/20160723124727_create_links.rb
+++ db/migrate/20160723124727_create_links.rb
@@ -1,4 +1,10 @@
 class CreateLinks < ActiveRecord::Migration
   def change
+    create_table :links do |t|
+      t.string :title, presence: true
+      t.timestamps null:false
+
+    end
+
   end
 end
