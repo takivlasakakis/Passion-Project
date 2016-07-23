3ae9db3be1bbd6109dab1aa444d273113e96faf8
TakiVlasakakis <email": null>
2016-07-23 12:30:01 -0700

    :sparkles: add schema and update user migration file with one field

--- db/migrate/20160716142558_create_users.rb
+++ db/migrate/20160716142558_create_users.rb
@@ -3,9 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name, presence: true
       t.string :email, presence: true, uniqueness: true
+      t.integer :age, presence:true
       t.string :password_hash, presence: true
-
-      t.integer :deck_id
 
       t.timestamps null: false
     end
