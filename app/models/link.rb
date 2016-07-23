105037374139c059724e0592ac3dd0a41faa62ee
TakiVlasakakis <email": null>
2016-07-23 13:37:55 -0700

    :sparkles: add associations to models

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,6 @@
 class Link < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :users
+  belongs_to :subjects
+
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,5 @@
 class Subject < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :users
+  has_many :links
 end
